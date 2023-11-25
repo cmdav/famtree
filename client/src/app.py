137079b3f2b4396f248a5e3c01b2edeeddb858e3
AloysiusC <49243613+AloysiusC@users.noreply.github.com>
2023-11-24 20:25:19 -0500

    updates

--- client/src/app.py
+++ client/src/app.py
@@ -1,24 +1,56 @@
 import base64
+import os
 from email.mime.text import MIMEText
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from requests import HTTPError
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google.auth.exceptions import RefreshError
+from googleapiclient.errors import HttpError
 
-SCOPES = [
-        "https://www.googleapis.com/auth/gmail.send"
-    ]
-flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-creds = flow.run_local_server(port=0)
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
-service = build('gmail', 'v1', credentials=creds)
-message = MIMEText('This is the body of the email')
-message['to'] = 'recipient@gmail.com'
-message['subject'] = 'Email Subject'
-create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+def main():
+    try:
+        creds = None
+        # The file token.json stores the user's access and refresh tokens and is
+        # created automatically when the authorization flow completes for the first time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json')
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                try:
+                    creds.refresh(Request())
+                except RefreshError as e:
+                    print(f"Error refreshing credentials: {e}")
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
 
-try:
-    message = (service.users().messages().send(userId="me", body=create_message).execute())
-    print(F'sent message to {message} Message Id: {message["id"]}')
-except HTTPError as error:
-    print(F'An error occurred: {error}')
-    message = None
+        service = build('gmail', 'v1', credentials=creds)
+
+        message = MIMEText('This is the body of the email')
+        to_address = 'aloysius397@gmail.com'
+        subject = 'Email Subject'
+
+        # Set the recipient in the 'To' field of the raw message
+        create_message = {
+            'raw': base64.urlsafe_b64encode(
+                f'To: {to_address}\nSubject: {subject}\n\nThis is the body of the email'.encode()
+            ).decode()
+        }
+
+        try:
+            sent_message = service.users().messages().send(userId="me", body=create_message).execute()
+            print(f'Sent message to {to_address} Message Id: {sent_message["id"]}')
+        except HttpError as error:
+            print(f'An error occurred: {error}')
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+if __name__ == '__main__':
+    main()
